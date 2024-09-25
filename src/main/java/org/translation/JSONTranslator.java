b161286e29f0e0ac1c172405c619c19449f9ada6
GuoYuHeJason <guoyu.he@mail.utoronto.ca>
2024-09-25 09:31:54 -0400

    JSONTranslator Finished Comments added

--- src/main/java/org/translation/JSONTranslator.java
+++ src/main/java/org/translation/JSONTranslator.java
@@ -5,9 +5,11 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * An implementation of the Translator interface which reads in the translation
@@ -15,7 +17,11 @@ import org.json.JSONArray;
  */
 public class JSONTranslator implements Translator {
 
-    // TODO Task: pick appropriate instance variables for this class
+    // countries = {"afg", "alb"...} all alpha3
+    private List<String> countries = new ArrayList<>();
+    // translations = [{"ar":"أفغانستان","bg":"Афганистан".....} {"ar":"ألبانيا","bg":"Албания"}
+    // JSON objects that is f(language code) -> countryName, for each country
+    private JSONArray translations = new JSONArray();
 
     /**
      * Constructs a JSONTranslator using data from the sample.json resources file.
@@ -37,8 +43,16 @@ public class JSONTranslator implements Translator {
 
             JSONArray jsonArray = new JSONArray(jsonString);
 
-            // TODO Task: use the data in the jsonArray to populate your instance variables
-            //            Note: this will likely be one of the most substantial pieces of code you write in this lab.
+            // this for loop makes the instance variables what's said in comments
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                final String country = jsonObject.getString("alpha3");
+                jsonObject.remove("alpha3");
+                jsonObject.remove("alpha2");
+                jsonObject.remove("id");
+                countries.add(country);
+                translations.put(jsonObject);
+            }
 
         }
         catch (IOException | URISyntaxException ex) {
@@ -48,21 +62,41 @@ public class JSONTranslator implements Translator {
 
     @Override
     public List<String> getCountryLanguages(String country) {
-        // TODO Task: return an appropriate list of language codes,
-        //            but make sure there is no aliasing to a mutable object
-        return new ArrayList<>();
+
+        List<String> languages = new ArrayList<>();
+        JSONObject temp = new JSONObject();
+
+        // Get the country in translations
+        // temp = the country JSONObject
+        int index = countries.indexOf(country);
+        temp = translations.getJSONObject(index);
+
+        // keys = language codes only, without actual translation
+        Iterator<String> keys = temp.keys();
+
+        // change type from iterator to list
+        while (keys.hasNext()) {
+            languages.add(keys.next());
+        }
+        return languages;
     }
 
     @Override
     public List<String> getCountries() {
-        // TODO Task: return an appropriate list of country codes,
-        //            but make sure there is no aliasing to a mutable object
-        return new ArrayList<>();
+        return countries;
     }
 
     @Override
-    public String translate(String country, String language) {
-        // TODO Task: complete this method using your instance variables as needed
-        return null;
+    public String translate(String countryCode, String languageCode) {
+        JSONObject temp = new JSONObject();
+
+        // Get the country in translations
+        // temp = the country JSONObject
+        int index = countries.indexOf(countryCode);
+        temp = translations.getJSONObject(index);
+
+        // temp can map languageCode to finalTranslation
+        String finalTranslation = temp.getString(languageCode);
+        return finalTranslation;
     }
 }
